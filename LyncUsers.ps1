98abe9ffe6509baee0bdcf5e9e2958357dd05368
Kriegersan <keith.r.b.nelson@gmail.com>
2017-06-29 10:51:06 -0400

    Delete LyncUsers.ps1

--- LyncUsers.ps1
+++
@@ -1,6 +0,0 @@
- $cred = Get-Credential
-
- invoke-command -computername leonard -credential $cred -authentication CredSSP -scriptblock {C:\users\iskrn1da\Desktop\Lync-Users.ps1}
-
- Write-host "Done!"
-
