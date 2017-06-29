ea70feedd6fc7dafaca392fe198492b1ca7a6754
Kriegersan <keith.r.b.nelson@gmail.com>
2017-06-29 10:49:26 -0400

    Delete Audrey1.ps1

--- Audrey1.ps1
+++
@@ -1,8 +0,0 @@
-$user = "admin\iskrn1da"
-$password= cat C:\securestring.txt | convertto-securestring
-$cred = new-object -typename System.Management.Automation.PSCredential `
-         -argumentlist $user, $password
-
-
-new-pssession -computername audrey -credential $cred -authentication CredSSP 
-
